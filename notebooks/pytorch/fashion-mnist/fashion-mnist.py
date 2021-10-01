9daed586f18d92a2604b4e14c4e0d0255839aa0a
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-10-01 12:10:50 +1000

    Refactor fashion-mnist.py

--- notebooks/pytorch/fashion-mnist/fashion-mnist.py
+++ notebooks/pytorch/fashion-mnist/fashion-mnist.py
@@ -1,4 +1,3 @@
-from numpy import mod
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -18,12 +17,13 @@ class NeuralNet(nn.Module):
         self.flatten = nn.Flatten()
         # The layers
         self.layers = nn.Sequential(
-            nn.Linear(in_features=28 * 28, out_features=512),
+            nn.Linear(in_features=28 * 28, out_features=256),
             nn.ReLU(),
-            nn.Linear(in_features=512, out_features=256),
+            nn.Linear(in_features=256, out_features=128),
             nn.ReLU(),
-            nn.Linear(in_features=256, out_features=10),
+            nn.Linear(in_features=128, out_features=64),
             nn.ReLU(),
+            nn.Linear(in_features=64, out_features=10),
         )
 
     def forward(self, x):
@@ -32,71 +32,91 @@ class NeuralNet(nn.Module):
         return logits
 
 
-def train_loop(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        X = X.to(device)
-        y = y.to(device)
-        pred = model(X)
-        loss = loss_fn(pred, y)
+class Trainer:
+    def __init__(
+        self,
+        train_data,
+        test_data,
+        learning_rate=1e-3,
+        batch_size=64,
+        epochs=10,
+        model=NeuralNet(),
+        optimizer=torch.optim.SGD,
+        loss_fn=nn.CrossEntropyLoss(),
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    ):
+        self.train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+        self.test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.device = device
+        self.model = model.to(device)
+        self.optimizer = optimizer(self.model.parameters(), lr=self.learning_rate)
+        self.loss_fn = loss_fn
+        print(self)
 
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    def train_loop(self, loader):
+        size = len(loader.dataset)
+        for batch, (X, y) in enumerate(self.train_loader):
+            # Compute prediction and loss
+            X = X.to(self.device)
+            y = y.to(self.device)
+            pred = self.model(X)
+            loss = self.loss_fn(pred, y)
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            # Backpropagation
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def test_loop(dataloader, model, loss_fn, device):
-    size = len(dataloader.dataset)
-    test_loss, correct = 0, 0
+    def test_loop(self, loader):
+        size = len(loader.dataset)
+        test_loss, correct = 0, 0
 
-    with torch.no_grad():
-        for X, y in dataloader:
-            X = X.to(device)
-            y = y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        with torch.no_grad():
+            for X, y in loader:
+                X = X.to(self.device)
+                y = y.to(self.device)
+                pred = self.model(X)
+                test_loss += self.loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        test_loss /= size
+        correct /= size
+        print(
+            f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+        )
+
+    def run(self):
+        for t in range(self.epochs):
+            print(f"Epoch {t+1}\n-------------------------------")
+            self.train_loop(self.train_loader)
+            self.test_loop(self.test_loader)
+        print("Done!")
+
+    def __repr__(self):
+        return f"DEVICE: Using {self.device}\nMODEL: {self.model}"
 
 
 def main():
     train_data = datasets.FashionMNIST(
-        root="data", train=True, download=True, transform=ToTensor()
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
     )
 
     test_data = datasets.FashionMNIST(
         root="data", train=False, download=True, transform=ToTensor()
     )
 
-    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
-    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"DEVICE: Using {device}")
-
-    model = NeuralNet().to(device)
-    print(f"MODEL: {model}")
-
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
-
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
-
-    epochs = 10
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(train_dataloader, model, loss_fn, optimizer, device)
-        test_loop(test_dataloader, model, loss_fn, device)
-    print("Done!")
+    trainer = Trainer(train_data=train_data, test_data=test_data)
+    trainer.run()
 
 
 if __name__ == "__main__":
